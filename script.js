6b512ca62af55f482d4a330b3688f04b704e408a
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-08 10:27:02 +0200

    Change default value in color picker

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let buttons = Array.from(document.querySelectorAll("button"));
 let numOfPressed = 0;
 buttons.forEach(button => button.addEventListener("click",function(){
-        button.classList.toggle("pressed");
-        for(let i = 0; i < buttons.length; i++){
-            if(button.classList.contains("pressed") && buttons[i].classList.contains("pressed") && buttons[i] != button){
-                buttons[i].classList.remove("pressed");
+        if(!button.classList.contains("clear")){
+            button.classList.toggle("pressed");
+            for(let i = 0; i < buttons.length; i++){
+                if(button.classList.contains("pressed") && buttons[i].classList.contains("pressed") && buttons[i] != button){
+                    buttons[i].classList.remove("pressed");
+                }
             }
         }
 }))
