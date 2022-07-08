52a49d651a4e6a20e7fff1b1b2945a2f45e3e3b5
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-08 18:19:20 +0200

    Add icons and rainbow mode

--- script.js
+++ script.js
@@ -1,73 +1,101 @@
 const defaultColor = "#FDDBFF";
 const defaultSize = 16;
-let buttons = Array.from(document.querySelectorAll("button"));
-let numOfPressed = 0;
-let body = document.querySelector("body");
-let currentColor = defaultColor;
-function resetGrid(){
-    console.log("Clear");
-}
-function colorMode(){
-    console.log("Change color");
-}
-function rainbowMode(){
-    console.log("Rainbow mode");
-}
-function eraser(){
-    console.log("Eraser");
-}
-function changeColor(e){
-    e.target.style.backgroundColor = "black";
-}
-buttons.forEach(button => button.addEventListener("click",function(){
-        if(!button.classList.contains("clear")){
-            button.classList.toggle("pressed");
-            if(button.classList.contains("pressed")){
-                switch(button.id){
-                    case "colorMode":
-                        colorMode();
-                        break;
-                    case "rainbowMode":
-                        rainbowMode();
-                        break;
-                    case "eraser":
-                        eraser();
-                        break;
-                }
-                for(let i = 0; i < buttons.length; i++){
-                    if(buttons[i].classList.contains("pressed") && buttons[i] != button){
-                        buttons[i].classList.remove("pressed");
-                    }
-                }
-            }
+const defaultMode = "color";
+let size = defaultSize;
 
-        }
-}))
-let colorpicker = document.getElementById("")
+let currentMode = defaultMode;
+let currentColor = defaultColor;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+let buttons = Array.from(document.querySelectorAll("button"));
+let colorpicker = document.getElementById("picker");
 let slider = document.getElementById("slider");
-let currentValue = slider.value;
 let output = document.getElementById("grid-dimension-prompt");
 let grid = document.getElementById("grid");
-function drawGrid(size){
-    let squareDimension = 512.0 / size;
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    for(let i = 0; i < size*size; i++){
-        console.log(22);
-        const cell = document.createElement("div");
-        cell.addEventListener('mouseover', changeColor);
-        cell.addEventListener('mousedown', changeColor);
-        grid.appendChild(cell);
-    }
-}
+
+colorpicker.oninput = (e) => setCurrentColor(e.target.value);
 slider.oninput = function() {
     grid.innerHTML = '';
     output.innerHTML = `${this.value} x ${this.value}`;
     size = this.value;
     drawGrid(size);
 }
+
+buttons.forEach(button => button.addEventListener("click",function(){
+    if(!button.classList.contains("clear")){
+        button.classList.toggle("pressed");
+        if(button.classList.contains("pressed")){
+            switch(button.id){
+                case "colorMode":
+                    currentMode = "color";
+                    break;
+                case "rainbowMode":
+                    currentMode = "rainbow";
+                    break;
+                case "eraser":
+                    currentMode = "eraser";
+                    break;
+            }
+            for(let i = 0; i < buttons.length; i++){
+                if(buttons[i].classList.contains("pressed") && buttons[i] != button){
+                    buttons[i].classList.remove("pressed");
+                }
+            }
+        }
+
+    }
+}))
+
+
+function resetGrid(){
+    grid.innerHTML = "";
+    console.log("Clear");
+
+    drawGrid(size);
+}
+
+function changeColor(e){
+    if(e.type == "mouseover" && mouseDown == false){
+       return;
+    }
+    if(currentMode == "color"){
+        e.target.style.backgroundColor = currentColor;
+    }else if(currentMode == "rainbow"){
+        let randomHexColor = Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.backgroundColor = "#" + randomHexColor;
+    }else{
+        e.target.style.backgroundColor = "#FFFFFF";
+    }
+}
+
+
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
+
+currentColor = colorpicker.value;
+
+
+
+
+function drawGrid(size){
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    //console.log(69);
+    for(let i = 0; i < size*size; i++){
+        //console.log(22);
+        const cell = document.createElement("div");
+        cell.addEventListener('mousedown', changeColor);
+        cell.addEventListener('mouseover', changeColor);
+        cell.classList.add("no-drag");
+        grid.appendChild(cell);
+    }
+}
+
 window.onload = () => {
     drawGrid(defaultSize);
     buttons[0].classList.add("pressed");
-    colorMode();
 }
