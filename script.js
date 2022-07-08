e14fa9b64b35dbc175e0f955baa5ded8e5415dab
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-08 10:18:57 +0200

    Make buttons click how they're supposed to

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let colorPicker = document.querySelector("color-picker").innerHTML;
+let buttons = Array.from(document.querySelectorAll("button"));
+let numOfPressed = 0;
+buttons.forEach(button => button.addEventListener("click",function(){
+        button.classList.toggle("pressed");
+        for(let i = 0; i < buttons.length; i++){
+            if(button.classList.contains("pressed") && buttons[i].classList.contains("pressed") && buttons[i] != button){
+                buttons[i].classList.remove("pressed");
+            }
+        }
+}))
