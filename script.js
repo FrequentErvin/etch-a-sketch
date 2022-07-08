829442ffbf1e2389bd47ec8853a48f20b0100c30
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-08 14:12:57 +0200

    Make draw mode - basic black

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function rainbowMode(){
 function eraser(){
     console.log("Eraser");
 }
-function changeColor(){
-
+function changeColor(e){
+    e.target.style.backgroundColor = "black";
 }
 buttons.forEach(button => button.addEventListener("click",function(){
         if(!button.classList.contains("clear")){
@@ -50,14 +50,13 @@ let output = document.getElementById("grid-dimension-prompt");
 let grid = document.getElementById("grid");
 function drawGrid(size){
     let squareDimension = 512.0 / size;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for(let i = 0; i < size*size; i++){
         console.log(22);
-        grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-        grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
         const cell = document.createElement("div");
         cell.addEventListener('mouseover', changeColor);
         cell.addEventListener('mousedown', changeColor);
-        cell.textContent = "";
         grid.appendChild(cell);
     }
 }
